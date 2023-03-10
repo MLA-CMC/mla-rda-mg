f91a07c61d0a25640448c5e41755daa69bbfa024
kek67197 <kek67197@cat056.msmyid.uga.edu>
2023-03-10 12:03:23 -0500

    Pages test

---
+++ docs/oxygen-webhelp/app/nav-links/json/tocId-d259e166.js
@@ -0,0 +1,1 @@
+define({"topics":[{"title":"Element Overview: MARC Bibliographic","shortdesc":"<p class=\"shortdesc\"\/>","href":"Content\/bib_element_overview_marc.html","attributes":{"data-id":"element_overview_marc_bibliographic"},"menu":{"hasChildren":true},"tocID":"element_overview_marc_bibliographic-d259e170","next":"element_overview_marc_bibliographic-d259e170"}]});
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {15:14,36:-1,9:25,31:-1,14:13,10:8,18:15,2:36,17:15,25:-1,4:5,6:37,5:36,33:-1,0:33,12:8,7:37,19:15,8:25,11:8,3:2,20:21,21:14,1:33,16:15,37:-1,22:14,13:25};
+});

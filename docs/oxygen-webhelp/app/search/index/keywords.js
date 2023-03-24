9f35e457d99d8d250a7ccde3f5db64d55dbabe54
kek67197 <kek67197@cat056.msmyid.uga.edu>
2023-03-24 17:15:19 -0400

    force reload of docs folder

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,37 @@
+define(function() {var keywords=[{w:"Interpreting",p:["p0"]},{w:"the",p:["p0","p1","p26"]},{w:"MLA",p:["p0","p1"]},{w:"Best",p:["p0","p26"]},{w:"Practices",p:["p0","p26"]},{w:"RDA",p:["p1","p25","p26","p27"]},{w:"Application",p:["p1"]},{w:"Profile",p:["p1"]},{w:"Aggregates",p:["p2"]},{w:"Authorized",p:["p3"]},{w:"Access",p:["p3","p6"]},{w:"Points",p:["p3","p6"]},{w:"for",p:["p3","p6","p7","p8","p16","p17","p18","p19","p26"]},{w:"Aggregating",p:["p3"]},{w:"Works",p:["p3","p6","p7","p28"]},{w:"and",p:["p3","p5","p6","p7","p14","p21","p26","p27","p28","p29"]},{w:"Expressions",p:["p3","p4","p5","p6","p7","p28"]},{w:"Treatment",p:["p4"]},{w:"of",p:["p4"]},{w:"Representative",p:["p4","p5"]},{w:"Expression",p:["p5","p10"]},{w:"Elements",p:["p5","p8","p10","p11","p12"]},{w:"Musical",p:["p6","p7","p28"]},{w:"Preferred",p:["p7"]},{w:"Titles",p:["p7"]},{w:"Guidance",p:["p8","p13","p25","p26"]},{w:"Specific",p:["p8"]},{w:"Element",p:["p9","p24"]},{w:"Overview:",p:["p9"]},{w:"MARC",p:["p9","p24"]},{w:"Bibliographic",p:["p9","p23"]},{w:"Manifestation",p:["p11"]},{w:"Work",p:["p12"]},{w:"Format-Specific",p:["p13"]},{w:"Electronic",p:["p14","p15","p16","p17","p18"]},{w:"Resources",p:["p14","p15","p16","p17","p18"]},{w:"Print",p:["p14","p21"]},{w:"Reproductions",p:["p14","p21"]},{w:"Format",p:["p15","p21"]},{w:"Guidance:",p:["p15","p21"]},{w:"General",p:["p16"]},{w:"Guidelines",p:["p16","p17","p18"]},{w:"All",p:["p16"]},{w:"Additional",p:["p17","p18"]},{w:"Direct-Access",p:["p17"]},{w:"Remote-Access",p:["p18"]},{w:"Special",p:["p19"]},{w:"Considerations",p:["p19"]},{w:"Scores",p:["p19"]},{w:"Original",p:["p20"]},{w:"Print-on-Demand",p:["p20"]},{w:"Publications",p:["p20"]},{w:"Photocopies",p:["p21"]},{w:"Provider-Neutral",p:["p22"]},{w:"Cataloging",p:["p22","p26"]},{w:"Description",p:["p23"]},{w:"Examples",p:["p24"]},{w:"by",p:["p24"]},{w:"Music",p:["p25","p26"]},{w:"Library",p:["p25","p26"]},{w:"Association",p:["p25","p26"]},{w:"Metadata",p:["p25","p26"]},{w:"Introduction",p:["p26"]},{w:"to",p:["p26"]},{w:"Using",p:["p26"]},{w:"MARC21",p:["p26"]},{w:"LRM",p:["p27"]},{w:"Concepts",p:["p27"]},{w:"Version",p:["p29"]},{w:"Citation",p:["p29"]},{w:"Information",p:["p29"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4];
+ph["p1"]=[1, 2, 5, 6, 7];
+ph["p2"]=[8];
+ph["p3"]=[9, 10, 11, 12, 13, 14, 15, 16];
+ph["p4"]=[17, 18, 19, 16];
+ph["p5"]=[19, 16, 15, 19, 20, 21];
+ph["p6"]=[10, 11, 12, 22, 14, 15, 16];
+ph["p7"]=[23, 24, 12, 22, 14, 15, 16];
+ph["p8"]=[25, 12, 26, 21];
+ph["p9"]=[27, 28, 29, 30];
+ph["p10"]=[20, 21];
+ph["p12"]=[32, 21];
+ph["p11"]=[31, 21];
+ph["p14"]=[34, 35, 15, 36, 37];
+ph["p13"]=[33, 25];
+ph["p16"]=[40, 41, 12, 42, 34, 35];
+ph["p15"]=[38, 39, 34, 35];
+ph["p18"]=[43, 41, 12, 45, 34, 35];
+ph["p17"]=[43, 41, 12, 44, 34, 35];
+ph["p19"]=[46, 47, 12, 48];
+ph["p21"]=[38, 39, 36, 37, 15, 52];
+ph["p20"]=[49, 50, 51];
+ph["p23"]=[30, 55];
+ph["p22"]=[53, 54];
+ph["p25"]=[58, 59, 60, 5, 61, 25];
+ph["p24"]=[29, 56, 57, 27];
+ph["p27"]=[66, 15, 5, 67];
+ph["p26"]=[62, 63, 1, 58, 59, 60, 3, 4, 12, 58, 54, 64, 5, 15, 65, 15, 5, 61, 25];
+ph["p29"]=[68, 15, 69, 70];
+ph["p28"]=[22, 14, 15, 16];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});

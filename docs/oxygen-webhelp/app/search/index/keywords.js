70e23a3fd563bef46a27e281b9a53798733d1ada
kek67197 <kek67197@cat056.msmyid.uga.edu>
2023-06-30 15:05:24 -0400

    Docs republish

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,33 @@
+define(function() {var keywords=[{w:"Guidance",p:["p0","p5","p12","p23"]},{w:"for",p:["p0","p12","p16","p21","p22"]},{w:"Specific",p:["p0"]},{w:"Elements",p:["p0","p2","p3","p4","p19"]},{w:"Element",p:["p1","p24"]},{w:"Overview:",p:["p1"]},{w:"MARC",p:["p1","p24"]},{w:"Bibliographic",p:["p1","p11"]},{w:"Expression",p:["p2","p19"]},{w:"Manifestation",p:["p3"]},{w:"Work",p:["p4"]},{w:"Format-Specific",p:["p5"]},{w:"Electronic",p:["p6","p7"]},{w:"Resources",p:["p6","p7"]},{w:"and",p:["p6","p9","p12","p16","p17","p19","p20","p21","p22","p25"]},{w:"Print",p:["p6","p9"]},{w:"Reproductions",p:["p6","p9"]},{w:"Format",p:["p7","p9"]},{w:"Guidance:",p:["p7","p9"]},{w:"Sound",p:["p8"]},{w:"Recordings",p:["p8"]},{w:"Photocopies",p:["p9"]},{w:"Provider-Neutral",p:["p10"]},{w:"Cataloging",p:["p10","p12"]},{w:"Description",p:["p11"]},{w:"Introduction",p:["p12"]},{w:"to",p:["p12"]},{w:"the",p:["p12","p13","p14"]},{w:"Music",p:["p12","p23"]},{w:"Library",p:["p12","p23"]},{w:"Association",p:["p12","p23"]},{w:"Best",p:["p12","p13"]},{w:"Practices",p:["p12","p13"]},{w:"Using",p:["p12"]},{w:"RDA",p:["p12","p14","p17","p23"]},{w:"MARC21",p:["p12"]},{w:"Metadata",p:["p12","p23"]},{w:"Interpreting",p:["p13"]},{w:"MLA",p:["p13","p14"]},{w:"Application",p:["p14"]},{w:"Profile",p:["p14"]},{w:"Aggregates",p:["p15"]},{w:"Authorized",p:["p16"]},{w:"Access",p:["p16","p21"]},{w:"Points",p:["p16","p21"]},{w:"Aggregating",p:["p16"]},{w:"Works",p:["p16","p20","p21","p22"]},{w:"Expressions",p:["p16","p18","p19","p20","p21","p22"]},{w:"LRM",p:["p17"]},{w:"Concepts",p:["p17"]},{w:"Treatment",p:["p18"]},{w:"of",p:["p18"]},{w:"Representative",p:["p18","p19"]},{w:"Musical",p:["p20","p21","p22"]},{w:"Preferred",p:["p22"]},{w:"Titles",p:["p22"]},{w:"Examples",p:["p24"]},{w:"by",p:["p24"]},{w:"Version",p:["p25"]},{w:"Citation",p:["p25"]},{w:"Information",p:["p25"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3];
+ph["p1"]=[4, 5, 6, 7];
+ph["p2"]=[8, 3];
+ph["p3"]=[9, 3];
+ph["p4"]=[10, 3];
+ph["p5"]=[11, 0];
+ph["p6"]=[12, 13, 14, 15, 16];
+ph["p7"]=[17, 18, 12, 13];
+ph["p8"]=[19, 20];
+ph["p9"]=[17, 18, 15, 16, 14, 21];
+ph["p10"]=[22, 23];
+ph["p12"]=[25, 26, 27, 28, 29, 30, 31, 32, 1, 28, 23, 33, 34, 14, 35, 14, 34, 36, 0];
+ph["p11"]=[7, 24];
+ph["p14"]=[27, 38, 34, 39, 40];
+ph["p13"]=[37, 27, 38, 31, 32];
+ph["p16"]=[42, 43, 44, 1, 45, 46, 14, 47];
+ph["p15"]=[41];
+ph["p18"]=[50, 51, 52, 47];
+ph["p17"]=[48, 14, 34, 49];
+ph["p19"]=[52, 47, 14, 52, 8, 3];
+ph["p21"]=[43, 44, 1, 53, 46, 14, 47];
+ph["p20"]=[53, 46, 14, 47];
+ph["p23"]=[28, 29, 30, 34, 36, 0];
+ph["p22"]=[54, 55, 1, 53, 46, 14, 47];
+ph["p25"]=[58, 14, 59, 60];
+ph["p24"]=[6, 56, 57, 4];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {11:-1,6:5,14:12,1:11,18:19,12:-1,22:20,10:6,3:0,20:-1,24:-1,15:17,8:5,19:17,7:6,2:0,5:11,13:12,4:0,25:-1,21:20,17:-1,9:6,0:11,16:15};
+});
